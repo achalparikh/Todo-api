e7b714880e562e927ee3e793c40e17a0912ffc09
achal.parikh13@gmail.com <achal.parikh@uoit.net>
2017-04-24 22:19:44 -0400

    New routes

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 var express = require ('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'meet for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'shopping',
+    completed: false
+}, {
+    id: 3, 
+    description: 'watch tv',
+    completed: true
+}];
+
 
 app.get('/', function(req, res) {
     res.send('Todo APP API');
+});
+
+//GET, get all todos, '/todos'
+app.get('/todos', function (req, res){
+    res.json(todos);
+});
+
+//GET, get one todo, '/todos/:id'
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id);
+
+    todos.forEach(function(todo) {
+        if (todoId === todo.id) {
+            res.json(todo);
+        } else 
+        res.status(404).send();
+    });
 });
 
 app.listen (PORT, function () {
