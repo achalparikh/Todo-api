65404ce9cce384eb2e8c6fbc8456050e9d9c803a
achal.parikh13@gmail.com <achal.parikh@uoit.net>
2017-04-27 22:42:57 -0400

    added function to update todo by id

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
-
-
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
     res.send('Todo APP API');
@@ -34,7 +32,7 @@ app.get('/todos/:id', function(req, res) {
     }
 });
 
-//POST request to create data  POST /todos
+//POST /todos/ (add todo to the todos array)
 app.post('/todos', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
@@ -50,7 +48,7 @@ app.post('/todos', function(req, res) {
     }
 });
 
-//DELETE /todos/:id
+//DELETE /todos/:id (delete todo by id)
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
 
@@ -64,6 +62,46 @@ app.delete('/todos/:id', function (req, res) {
         res.status(404).send();
     }
 }); 
+
+//PUT /todos/:id (update todo by id)
+app.put('/todos/:id', function (req, res) {
+
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    //if no todo is found, quit
+    if (!matchedTodo) {
+        console.log("WHAT WHY");
+        return res.status(404).send();
+    }
+
+    //verify the completed attribute in the request
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        res.status(400).send();
+    }
+
+    //verify the description attribute in the request
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.send(matchedTodo);
+
+});
+
+
+
+
+//Event listener 
 
 app.listen (PORT, function () {
     console.log('express listening on port ' + PORT);
